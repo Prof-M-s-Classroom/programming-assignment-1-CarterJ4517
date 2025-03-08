bcd4f7261219257416ff6f0a22c696eeb377c10b
CarterJ4517 <github@carterj.us>
2025-03-08 13:08:16 -0800

    commit

--- distance.py
+++ distance.py
@@ -1,11 +1,14 @@
+import datetime
+
 class Distance:
     """
     Class to represent a distance measurement with timestamp.
     """
     def __init__(self, distance):
         """Initialize the Distance object with distance and timestamp."""
-        pass
+        self.distance = distance
+        self.time = datetime.datetime.now().timestamp()
 
     def __str__(self):
         """Return a formatted string representation of the Distance object."""
-        pass
+        return f"Distance of {self.distance} taken at {self.time}"
